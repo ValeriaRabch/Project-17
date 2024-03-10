ad6e65fc0367d2f5b1bed64bcd9e47ab019e480f
ValeriaRabch <rabchenukv@gmail.com>
2024-03-10 21:28:07 +0200

    Add coment

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 	//завдання 1
 
-	FILE* file1, * file2;
+	/*FILE* file1, * file2;
 	file1 = fopen("D:\\Valeria\\file.txt", "r");
 	file2 = fopen("D:\\Valeria\\file2.txt", "r");
 
@@ -90,7 +90,7 @@ int main() {
 	}
 
 	fclose(file1);
-	fclose(file2); 
+	fclose(file2); */
 
 	return 0;
 }
