051d7398a362b90033b81c5c6b36ab6bb23b7a2d
ValeriaRabch <rabchenukv@gmail.com>
2024-03-10 21:26:56 +0200

    Create file1, file2

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 int main() {
+	//завдання 1
+
+	FILE* file1, * file2;
+	file1 = fopen("D:\\Valeria\\file.txt", "r");
+	file2 = fopen("D:\\Valeria\\file2.txt", "r");
+
+
+	fclose(file1);
+	fclose(file2); 
 
 	return 0;
 }
